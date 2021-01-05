15c8df6d463c4ee1a725db1985c18f4d7043616a
Jesse Byers <jesse.s.byers@gmail.com>
2021-01-05 15:09:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,10 +12,11 @@ end
 
 def selection(num)
   if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+     output = "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+     output = "HAM HAM HAM IN MY TUMMY"
   end
+  output
 end
 
 def runner
